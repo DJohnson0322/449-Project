ef4969bb24190b0c99724b7b2c79201458701fe3
DJohnson0322 <143641485+DJohnson0322@users.noreply.github.com>
2025-11-04 16:55:22 -0600

    Update TestNoughtMoves code

--- Sprint_2/sosgame.test/TestNoughtMoves.java
+++ Sprint_2/sosgame.test/TestNoughtMoves.java
@@ -10,26 +10,26 @@ public class TestNoughtMoves {
     @Before
     public void setUp() throws Exception {
         board = new Board(3, "Simple Game");
-        board.makeMove(1, 1, 'S');
+        board.makeMove(1, 1, 'S', 'R');
     }
     @Test
     public void testValidMoveVacantCell() {
-        board.makeMove(0, 0, 'O');
+        board.makeMove(0, 0, 'O', 'B');
         assertEquals("Cell (0,0) should now contain 'O'", 'O', board.getCell(0, 0));
     }
     @Test
     public void testMoveNonVacantCell() {
-        board.makeMove(1, 1, 'O');
+        board.makeMove(1, 1, 'O', 'R');
         assertEquals("Occupied cell (1,1) should still be 'S'", 'S', board.getCell(1, 1));
     }
     @Test
     public void testInvalidRowMove() {
-        board.makeMove(5, 0, 'O');
+        board.makeMove(5, 0, 'O', 'B');
         assertEquals("Invalid row should not affect valid cells", ' ', board.getCell(0, 0));
     }
     @Test
     public void testInvalidColumnMove() {
-        board.makeMove(0, 5, 'O');
+        board.makeMove(0, 5, 'O', 'R');
         assertEquals("Invalid column should not affect valid cells", ' ', board.getCell(0, 0));
     }
 }
