6fdc5049bc690c439cb5f82a5d574afc88098caa
DJohnson0322 <143641485+DJohnson0322@users.noreply.github.com>
2025-11-04 16:55:03 -0600

    Update TestCrossMoves code

--- Sprint_2/sosgame.test/TestCrossMoves.java
+++ Sprint_2/sosgame.test/TestCrossMoves.java
@@ -24,27 +24,27 @@ public class TestCrossMoves {
 
     @Test
     public void testValidMoveVacantCell() {
-        board.makeMove(0, 0, 'S');
+        board.makeMove(0, 0, 'S', 'R');
         assertEquals("Cell (0,0) should now contain 'S'", 'S', board.getCell(0, 0));
     }
 
     @Test
     public void testMoveOnOccupiedCell() {
-        board.makeMove(0, 0, 'S');
+        board.makeMove(0, 0, 'S', 'B');
         char before = board.getCell(0, 0);
-        board.makeMove(0, 0, 'O'); 
+        board.makeMove(0, 0, 'O', 'B'); 
         assertEquals("Cell (0,0) should remain 'S'", before, board.getCell(0, 0));
     }
 
     @Test
     public void testInvalidRowMove() {
-        board.makeMove(5, 0, 'S');
+        board.makeMove(5, 0, 'S', 'R');
         assertEquals("Out-of-bounds row should not modify board", ' ', board.getCell(0, 0));
     }
 
     @Test
     public void testInvalidColumnMove() {
-        board.makeMove(0, 5, 'O');
+        board.makeMove(0, 5, 'O', 'B');
         assertEquals("Out-of-bounds column should not modify board", ' ', board.getCell(0, 0));
     }
 }
