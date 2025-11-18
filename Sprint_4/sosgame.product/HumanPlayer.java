9694fa09bdc5e14da9da85c4a7670f87db625d56
DJohnson0322 <143641485+DJohnson0322@users.noreply.github.com>
2025-11-18 16:22:10 -0600

    Create HumanPlayer.java

---
+++ Sprint_4/sosgame.product/HumanPlayer.java
@@ -0,0 +1,13 @@
+package sosgame.product;
+
+public class HumanPlayer extends Player {
+
+    public HumanPlayer(char id) {
+        super(id);
+    }
+
+    @Override
+    public Move getMove(BaseGame game) {
+        return null;
+    }
+}
