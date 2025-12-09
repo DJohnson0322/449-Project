39673fa4377791c3f0dcfc195d06c742bbc73d15
DJohnson0322 <143641485+DJohnson0322@users.noreply.github.com>
2025-12-09 16:33:37 -0600

    Create Move.java

---
+++ Sprint_5/sosgame.product/Move.java
@@ -0,0 +1,12 @@
+package sosgame.product;
+
+public class Move {
+    public int r, c;
+    public char letter;
+
+    public Move(int r, int c, char letter) {
+        this.r = r;
+        this.c = c;
+        this.letter = letter;
+    }
+}
